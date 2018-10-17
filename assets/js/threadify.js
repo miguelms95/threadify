972b9c0b3fdf354e0ef439df2ce5f9aa70fee727
Miguel Martínez Serrano <miguelms.ovi@gmail.com>
2018-10-17 02:00:45 +0200

    Version 1.0 - works, copy and split text without wordwrap.
    Next step: word wrap

--- assets/js/threadify.js
+++ assets/js/threadify.js
@@ -1,34 +1,57 @@
 var fullText = '';
 var numChars = 0;
+var parts_text = []
 
-function updateNumChars(){
+function updateNumChars() {
     numChars = document.getElementById("full-text").value.length
     document.getElementById("num_characters").innerHTML = numChars
 }
 
-$('#full-text').bind('input propertychange', function() {
+$('#full-text').bind('input propertychange', function () {
     updateNumChars()
 });
 
 updateNumChars()
 
 $('#bt-submit-text').click(function () {
-   fullText = $('#full-text').val();
-   //console.log(fullText);
+    fullText = $('#full-text').val();
     splitter(fullText)
 });
 
-function splitter(string){
-    var numTweets = string.length/280
+function splitter(string) {
+    var numTweets = string.length / 280
     var realTweets = 0;
-    if(Math.round(numTweets) < numTweets)
-        realTweets = Math.round(numTweets) + 1
-    
-    console.log("number of tweets needed: " + numTweets + ". Rounded: " + Math.round(numTweets) + ". Real needed: " +realTweets )
 
-    parts = string.split(' ');
-    console.log(parts);
-    
-    console.log(string.substr(0,280))
-    
+    if (Math.round(numTweets) < numTweets)
+        realTweets = Math.round(numTweets) + 1
+    else
+        realTweets = Math.round(numTweets)
+
+    console.log("Total characters / 280 : " + numTweets + ". Rounded: " + Math.round(numTweets) + ". Real tweets needed: " + realTweets)
+
+    // With word wrap. I've to improve this later.
+
+    var x = 0;
+    var y = 280;
+
+
+
+    while (string.substr(x, y) != "") {
+        parts_text.push(string.substr(x, y))
+        x += 280
+        y += 280
+    }
+
+    for (i = 0; i < realTweets; i++) {
+        $("#text-area-results").append('<textarea rows="4" cols="30" id="text_result_' + i + '" class="form-control result_textarea">' + parts_text[i] + '</textarea>' + 
+        '<button class="btn btn-dark bt_result" type="submit" id="bt-copy-' + i + '" onclick="copy(' + i + ')"> Copy text <i class="fa fa-copy"></i></button>')
+    }
+    // $("#text_result_1").val( parts_text[0] )
+
+    console.log(parts_text)
+}
+
+function copy(i){
+    $("#text_result_"+i).select();
+    document.execCommand("copy");
 }
